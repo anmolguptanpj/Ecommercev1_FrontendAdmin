d1b80551a7775b9ef1b6e10883a2f3f87e975134
anmolguptanpj <anmolguptanpj282@gmail.com>
2025-11-14 11:18:51 +0530

    frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,43 @@
 import React from 'react'
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+import Login from './Pages/Login';
+import Orders from './Pages/Orders';
+import Sales from './Pages/Sales';
+import Payments from './Pages/Payments';
+import Returns from './Pages/Returns';
+import Dashboard from './Pages/Dashboard';
+import Suppliers from './Pages/Suppliers';
+import Staff from './Pages/Staff';
+import Products from './Pages/Products';
 
 function App() {
+
+
+  const PublicLayout = () => {
   return (
-    <div>App</div>
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+  return (
+   <Router>
+    <Routes>
+           <Route path="/" element= {<Login />}/>
+            <Route path="/dashboard" element= {<Dashboard />}/>
+           <Route path="/staff" element= {<Staff />}/>
+            <Route path="/orders" element= {<Orders/>}/>
+           <Route path="/products" element= {<Products/>}/>
+           <Route path="/customer" element= {<Customer />}/>
+            <Route path="payments/" element= {<Payments />}/>
+           <Route path="/returns" element= {<Returns/>}/>
+            <Route path="/sales" element= {<Sales />}/>
+           
+            <Route path="/suppliers" element= {<Suppliers />}/>
+    </Routes>
+   </Router>
   )
 }
 
---
+++ src/store/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+
+
+configureStore({
+    reducer:{
+
+    },
+});
+
+export default Store;
